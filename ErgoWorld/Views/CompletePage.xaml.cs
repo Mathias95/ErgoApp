45fd5ad33e3e594596b9afc6213e4144ce526b9b
Mathias95 <mathiasrjacobsen@gmail.com>
2018-12-30 22:24:18 +0100

    Video Player function added + Global styles

---
+++ ErgoWorld/Views/CompletePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ErgoWorld.Views
+{
+    public partial class CompletePage : ContentPage
+    {
+        public CompletePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
